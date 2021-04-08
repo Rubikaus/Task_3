65e6a6659c792d30e68f97fe612f13e336d08809
Sergey <yaskin.serzh@mail.ru>
2021-04-08 22:54:42 +0500

    Сортировщик музыкальных файлов

--- add.py
+++ add.py
@@ -48,12 +48,22 @@ def get_tags(track_list, src_dir=os.getcwd(), dst_dir=os.getcwd()):
             print("Отсутсвует тег у файла", music)
 
 
-parser = argparse.ArgumentParser(description = "Сортировка музыкальных файлов")
-parser.add_argument("--src", help = "Исходный путь с музыкой", default = "", required = True)
-parser.add_argument("--dst", help = "Конечный путь с отсортированной музыкой", default = "", required = True)
+parser = argparse.ArgumentParser(description="Сортировка музыкальных файлов")
+parser.add_argument("--src", help="Исходный путь с музыкой", default="", required=True)
+parser.add_argument("--dst", help="Конечный путь с отсортированной музыкой", default="", required=True)
 args = parser.parse_args()
 src_dir = args.src
 dst_dir = args.dst
+if not os.path.exists(src_dir):
+    print("Не существует введенного исходного пути")
+    exit(0)
+if not os.path.exists(dst_dir):
+    print("Не существует введенного конечного пути")
+    exit(0)
+if not os.access(src_dir, os.W_OK) and not os.access(src_dir, os.R_OK):
+    print("Нет прав доступа к исходной директории")
+if not os.access(dst_dir, os.W_OK) and not os.access(dst_dir, os.R_OK):
+    print("Нет прав доступа к конечной директории")
 
 var = read_tracks(src_dir)
 get_tags(var, src_dir, dst_dir)
